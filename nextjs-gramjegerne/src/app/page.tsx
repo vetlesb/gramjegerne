a1298553d798edc8299ee5b57d4e91de441d3289
Vetle Sandvik Berre <vetle@berre.no>
2024-10-24 16:40:00 +0200

    Versioning untracked files

--- nextjs-gramjegerne/src/app/page.tsx
+++ nextjs-gramjegerne/src/app/page.tsx
@@ -1,27 +1,46 @@
 import Link from "next/link";
 import { type SanityDocument } from "next-sanity";
-
+import imageUrlBuilder from '@sanity/image-url';
 import { client } from "@/sanity/client";
+
+
+// Get a pre-configured url-builder from your sanity client
+const builder = imageUrlBuilder(client)
+
+// Then we like to make a simple function like this that gives the
+// builder an image and returns the builder for you to specify additional
+// parameters:
+function urlFor(source) {
+  return builder.image(source)
+}
 
 const ITEMS_QUERY = `*[
   _type == "item"
-  && defined(slug.current)
-]|order(publishedAt desc)[0...12]{name, image, category, size, weight, quantity, calories}`;
+]{name, slug, image, category, size, weight, quantity, calories}`;
 
 const options = { next: { revalidate: 30 } };
 
 export default async function IndexPage() {
   const items = await client.fetch<SanityDocument[]>(ITEMS_QUERY, {}, options);
-
+console.log (items)
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
-      <h1 className="text-4xl font-bold mb-8">items</h1>
+      <h1 className="text-xl font-bold mb-8">items</h1>
       <ul className="flex flex-col gap-y-4">
         {items.map((item) => (
-          <li className="hover:underline" key={item._id}>
-            <Link href={`/${item.slug.current}`}>
-              <h2 className="text-xl font-semibold">{item.name}</h2>
-            </Link>
+          <li key={item._id}>
+        
+              <img src={urlFor(item.image).width(500).url()} />
+              <h2 className="text-4xl font-semibold">{item.name}</h2>
+              <p className="text-s">Category: {item.category.category}</p>
+              <p className="text-s">Size: {item.size}</p>
+              <p className="text-s">Quantity: {item.quantity}</p>
+              <p className="text-s">Weight: {item.weight.weight} {item.weight.unit}</p>
+              <p className="text-s">Slug: {item.slug.slug}</p>
+            
+              
+   
+     
           </li>
         ))}
       </ul>
--- studio-gramjegerne/schemaTypes/itemType.ts
+++ studio-gramjegerne/schemaTypes/itemType.ts
@@ -10,6 +10,16 @@ export const itemType = defineType({
       type: 'string',
     }),
     defineField({
+      title: 'slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+      },
+    }),
+    defineField({
       name: 'image',
       type: 'image',
     }),
