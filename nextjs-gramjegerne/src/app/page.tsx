ad320bdbb59735ccca5e68ef8a3a27d6f00f2b25
Vetle Sandvik Berre <vetle@berre.no>
2024-10-29 14:15:19 +0100

    modified:   nextjs-gramjegerne/src/app/lists/page.tsx 	modified:   nextjs-gramjegerne/src/app/page.tsx

--- nextjs-gramjegerne/src/app/page.tsx
+++ nextjs-gramjegerne/src/app/page.tsx
@@ -10,7 +10,7 @@ const builder = imageUrlBuilder(client)
 // Then we like to make a simple function like this that gives the
 // builder an image and returns the builder for you to specify additional
 // parameters:
-function urlFor(source:any) {
+function urlFor(source:unknown) {
   return builder.image(source)
 }
 
