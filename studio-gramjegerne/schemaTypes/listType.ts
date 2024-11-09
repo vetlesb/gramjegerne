b5f68a5cf1da4f25a53d6c90ced6db3fc4714d14
Vetle Sandvik Berre <vetle@berre.no>
2024-11-10 00:38:35 +0100

    modified:   studio-gramjegerne/schemaTypes/listType.ts

--- studio-gramjegerne/schemaTypes/listType.ts
+++ studio-gramjegerne/schemaTypes/listType.ts
@@ -2,38 +2,50 @@ import {defineField, defineType} from 'sanity'
 
 export const listType = defineType({
   name: 'list',
-  title: 'list',
+  title: 'List',
   type: 'document',
   fields: [
     defineField({
       name: 'name',
       type: 'string',
+      title: 'Name',
     }),
     defineField({
-      title: 'slug',
       name: 'slug',
       type: 'slug',
+      title: 'Slug',
       options: {
         source: 'name',
-        maxLength: 200, // will be ignored if slugify is set
+        maxLength: 200, // Will be ignored if slugify is set
         slugify: (input) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
       },
     }),
     defineField({
       name: 'image',
       type: 'image',
+      title: 'Image',
     }),
     defineField({
       name: 'days',
       type: 'number',
+      title: 'Days',
     }),
     defineField({
       name: 'weight',
       type: 'number',
+      title: 'Weight',
     }),
     defineField({
       name: 'participants',
       type: 'number',
+      title: 'Participants',
+    }),
+    // Added 'items' field to store references to item documents
+    defineField({
+      name: 'items',
+      type: 'array',
+      title: 'Items',
+      of: [{type: 'reference', to: [{type: 'item'}]}],
     }),
   ],
 })
