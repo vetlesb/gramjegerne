c691d91cc7a5561a4bb623ba641e585740b9a2bd
Vetle Sandvik Berre <vetle@berre.no>
2024-11-10 14:44:05 +0100

    modified:   nextjs-gramjegerne/src/sanity/client.ts

--- nextjs-gramjegerne/src/sanity/client.ts
+++ nextjs-gramjegerne/src/sanity/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   token: process.env.SANITY_API_TOKEN,
-  useCdn: true, // or false depending on your needs
+  useCdn: false,
   apiVersion: "2023-01-01",
 });
