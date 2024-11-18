353407dfe85f0d56cf744f332638e84c36914535
Vetle Sandvik Berre <vetle@berre.no>
2024-11-18 23:33:05 +0100

    Signin

--- nextjs-gramjegerne/src/app/auth/signin/page.tsx
+++ nextjs-gramjegerne/src/app/auth/signin/page.tsx
@@ -25,14 +25,14 @@ export default function SignIn() {
 
   // Show sign in form
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center min-h-screen justify-center items-center">
       <div className="w-full space-y-8">
         <h2 className="signin-logo text-accent text-center">Gramjegerne</h2>
 
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col justify-center items-center">
           <button
             onClick={() => signIn("google", { callbackUrl: "/" })}
-            className="group flex justify-center items-center gap-3 rounded-md "
+            className="button-primary group flex justify-center items-center gap-3 rounded-md bg-dimmed"
           >
             <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
               <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
