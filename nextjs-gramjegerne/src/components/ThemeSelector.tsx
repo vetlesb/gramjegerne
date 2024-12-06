7c6f7a4dcd3b58b24c16485b25da796e868298fb
Vetle Sandvik Berre <vetle@berre.no>
2024-12-06 15:53:21 +0100

    Bug fixes

--- nextjs-gramjegerne/src/components/ThemeSelector.tsx
+++ nextjs-gramjegerne/src/components/ThemeSelector.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import Icon from "@/components/Icon";
+
 export function ThemeSelector() {
   const { theme, setTheme } = useTheme();
 
@@ -18,18 +19,17 @@ export function ThemeSelector() {
     { value: "yellow", label: "Høst", icon: "leaf" },
   ] as const;
 
+  const currentTheme = themes.find((t) => t.value === theme);
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="menu-theme text-lg border-0 outline-none focus:outline-none rounded-md flex items-center justify-center gap-x-2">
-        <Icon
-          width={24}
-          height={24}
-          name={themes.find((t) => t.value === theme)?.icon ?? "tree"}
-        />
+      <DropdownMenuTrigger className="menu-theme text-lg border-0 outline-none focus:outline-none rounded-md flex items-center justify-center gap-x-1 md:w-28">
+        <Icon width={24} height={24} name={currentTheme?.icon ?? "tree"} />
+        <span>{currentTheme?.label}</span>
       </DropdownMenuTrigger>
       <DropdownMenuContent
-        align="start"
-        className="flex flex-col bg-primary gap-y-4 mt-3 p-4 bg-dimmed border-0"
+        align="center"
+        className="flex flex-col bg-primary gap-y-2 mt-3 p-4 bg-dimmed border-0"
       >
         {themes.map((t) => (
           <DropdownMenuItem
@@ -37,7 +37,7 @@ export function ThemeSelector() {
             onClick={() => setTheme(t.value)}
             className="text-lg gap-x-1"
           >
-            <Icon name={t.icon} />
+            <Icon name={t.icon} width={24} height={24} />
             {t.label}
             {theme === t.value}
           </DropdownMenuItem>
