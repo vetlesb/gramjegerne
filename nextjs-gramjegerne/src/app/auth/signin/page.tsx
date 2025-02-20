0ea8937fb2f023cf154715a388f9fe288515fadd
Vetle Sandvik Berre <vetle@berre.no>
2025-02-20 10:12:28 +0100

    Login button fix

--- nextjs-gramjegerne/src/app/auth/signin/page.tsx
+++ nextjs-gramjegerne/src/app/auth/signin/page.tsx
@@ -34,7 +34,7 @@ export default function SignIn() {
         <div className="flex flex-col justify-center items-center">
           <button
             onClick={() => signIn('google', {callbackUrl: '/'})}
-            className="button-primary group flex justify-center items-center gap-3 rounded-md bg-dimmed"
+            className="button-primary group flex justify-center items-center gap-3 rounded-md"
           >
             <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
               <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
