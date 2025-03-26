a7a64c33f00b141ea2179ea261534dfb40cacf8c
Vetle Sandvik Berre <vetle@berre.no>
2025-03-26 19:01:59 +0100

    Hide footer when signed out

--- nextjs-gramjegerne/src/app/layout.tsx
+++ nextjs-gramjegerne/src/app/layout.tsx
@@ -12,6 +12,7 @@ import {Toaster} from 'sonner';
 import Link from 'next/link';
 import './globals.css';
 import {Providers} from './providers';
+import {ProtectedRoute} from '@/components/auth/ProtectedRoute';
 
 const apfel = localFont({
   src: [
@@ -42,23 +43,25 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
           <Providers>
             {!isAuthPage && <Navbar />}
             {children}
-            <div className="flex flex-col md:flex-row justify-between">
-              <div className="text-center text-2xl text-accent pb-8 flex flex-row gap-1 p-8 justify-start">
-                Gramjegerne
+            <ProtectedRoute>
+              <div className="flex flex-col md:flex-row justify-between">
+                <div className="text-center text-2xl text-accent pb-8 flex flex-row gap-1 p-8 justify-start">
+                  Gramjegerne
+                </div>
+                <div className="text-center text-lg pb-8 flex flex-col gap-y-4 md:flex-row gap-1 p-8 justify-end">
+                  <ThemeSelector />
+                  <Link href="mailto:gramjegerne@gmail.com" className="menu-item">
+                    Contact us
+                  </Link>{' '}
+                  <button
+                    onClick={() => signOut()}
+                    className="menu-item text-lg flex items-center w-full md:w-auto gap-x-1 justify-center"
+                  >
+                    Log out
+                  </button>
+                </div>
               </div>
-              <div className="text-center text-lg pb-8 flex flex-col gap-y-4 md:flex-row gap-1 p-8 justify-end">
-                <ThemeSelector />
-                <Link href="mailto:gramjegerne@gmail.com" className="menu-item">
-                  Contact us
-                </Link>{' '}
-                <button
-                  onClick={() => signOut()}
-                  className="menu-item text-lg flex items-center w-full md:w-auto gap-x-1 justify-center"
-                >
-                  Log out
-                </button>
-              </div>
-            </div>
+            </ProtectedRoute>
           </Providers>
         </ThemeProvider>
         <Toaster />
